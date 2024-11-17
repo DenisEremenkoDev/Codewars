6ef5dae3d2f8d0c2ff71416ff45b0b4d2b277c32
DenisEremenkoDev <Deniseremenkodev@gmail.com>
2024-11-17 19:45:06 +0300

    task

--- 8kyu/training.js
+++ 8kyu/training.js
@@ -1,3 +1,13 @@
 function animal(obj) {
     return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
+
+
+
+
+// напиши функцию animal, принимающую 1 параметр: objнапример, так:
+
+// {name:"dog",legs:4,color:"white"}
+// и вернуть строку вроде этой:
+
+// "This white dog has 4 legs."
